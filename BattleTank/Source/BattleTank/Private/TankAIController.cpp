9f2d09a48bf041d0920737d27da330263a9df09f
DESKTOP-JO03LDA\Justin <geeziegames@gmail.com>
2017-02-23 00:04:10 -0800

    Added AIs that target player

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankAIController.h"
+
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ATank* ControlledAI = GetControlledAI();
+	if (!ControlledAI)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI possessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI controller possessing %s"), *(ControlledAI->GetName()));
+	}
+
+	ATank* PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI can't find player"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s targetting player tank (%s)"), *(ControlledAI->GetName()), *PlayerTank->GetName());
+	}
+}
+
+ATank* ATankAIController::GetControlledAI() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());	
+}
